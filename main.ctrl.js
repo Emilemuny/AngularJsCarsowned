3feaaea238ded4b043556e2c1cb89d68a17cb253
emilemuny <emilemuny007@gmail.com>
2015-02-03 01:05:43 -0800

    First AngularJs car owned committ

---
+++ main.ctrl.js
@@ -0,0 +1,68 @@
+
+angular.module('app').controller("MainController", function(){
+  var vm= this; 
+  vm.title = 'Cars I owned throught out the years';
+  vm.searchInput = '';
+  vm.shows = [
+{
+  title: 'Infiniti G35',
+year: 2005,
+favorite: true
+},
+{
+  title: 'Toyota Camry',
+year: 2002,
+favorite: false
+},
+{
+  title: 'Subaru',
+  year: 1999,
+  favorite: false
+},
+{
+  title: 'Benz Mercedes',
+  year: 2004,
+  favorite: true
+},
+{
+  title: 'Nissan',
+  year: 2005,
+  favorite: false
+}
+];
+
+vm.orders = [
+{
+  id: 1,
+    title:'Year Ascending',
+    key: 'year',
+    reverse: false
+},
+{
+  id: 2,
+  title: 'Year Descending',
+  key: 'year',
+  reverse: true
+},
+{
+  id: 3,
+  title: 'Title Ascending',
+  key: 'year',
+  reverse: true
+},
+{
+  id: 4,
+  title: 'Title Ascending',
+  key: 'title',
+  reverse: true
+}
+
+];
+ vm.order = vm.orders[0];
+  vm.new = {};
+  vm.addShow= function(){
+    vm.shows.push(vm.new);
+    vm.new = {};
+};
+});
+
